439027197a6cb2593596151d0a80e051b5ae708b
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2025-11-01 06:38:32 +0000

    Chatbot working fine

--- app.py
+++ app.py
@@ -36,13 +36,12 @@ y= tags
 clf.fit(x,y)
 
 def chatbot(input_text):
-    input_text =vectorizer.transform([input_text])
-    tag = clf.predict(input_text)[0]
+    vectorized_text = vectorizer.transform([input_text])
+    tag = clf.predict(vectorized_text)[0]
     for intent in intents:
         if intent["tag"] == tag:
             return random.choice(intent["responses"])
-            return response
-        return "Cant answer the question"
+    return "Can't answer the question yet"
 
 counter = 0
 
